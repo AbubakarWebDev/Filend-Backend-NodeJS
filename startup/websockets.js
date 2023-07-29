1451dab3a691c0408fb1d469f15b0177a2e1d7a8
Muhammad Abubakar <90tricks90@gmail.com>
2023-07-29 14:53:55 +0500

    Adding websockets implementation for filend

--- startup/websockets.js
+++ startup/websockets.js
@@ -6,9 +6,13 @@ module.exports = function (server) {
         },
     });
 
+    const chatNamespace = io.of("/chat");
+    const webRTCNamespace = io.of("/webRTC");
+
+
     const onlineUsers = {};
 
-    io.on('connection', (socket) => {
+    chatNamespace.on('connection', (socket) => {
         const socketId = socket.id;
         console.log(`A user with the id: ${socketId} is connected`);
 
@@ -17,7 +21,7 @@ module.exports = function (server) {
 
             onlineUsers[userId] = socketId;
 
-            io.emit('onlineUsers', onlineUsers);
+            chatNamespace.emit('onlineUsers', onlineUsers);
         });
 
         socket.on('joinChat', (chatId) => {
@@ -29,7 +33,7 @@ module.exports = function (server) {
 
             [...chat.users, ...chat.groupAdmins].forEach((user) => {
                 if (user._id !== userId) {
-                    io.to(user._id).emit('joinGroupChat', chat);
+                    chatNamespace.to(user._id).emit('joinGroupChat', chat);
                 }
             });
         });
@@ -37,31 +41,31 @@ module.exports = function (server) {
         socket.on('updateGroupChat', ({ chat, userId, addedUser, removedUser }) => {
             [...chat.users, ...chat.groupAdmins].forEach((user) => {
                 if (user._id !== userId) {
-                    io.to(user._id).emit('updatedGroupChat', chat);
+                    chatNamespace.to(user._id).emit('updatedGroupChat', chat);
                 }
             });
 
             removedUser.forEach((user) => {
-                io.to(user._id).emit('deleteGroupChat', chat._id);
+                chatNamespace.to(user._id).emit('deleteGroupChat', chat._id);
             });
 
             addedUser.forEach((user) => {
-                io.to(user._id).emit('joinGroupChat', chat);
+                chatNamespace.to(user._id).emit('joinGroupChat', chat);
             });
         });
 
         socket.on('typing', ({ chatId, user }) => {
-            io.to(chatId).except(user._id).emit('startTyping', { chatId, user });
+            chatNamespace.to(chatId).except(user._id).emit('startTyping', { chatId, user });
         });
 
         socket.on('typingOff', ({ chatId, user }) => {
-            io.to(chatId).except(user._id).emit('stopTyping', { chatId, user });
+            chatNamespace.to(chatId).except(user._id).emit('stopTyping', { chatId, user });
         });
 
         socket.on('sendMessage', (message) => {
             [...message.chat.users, ...message.chat.groupAdmins].forEach((user) => {
                 if (user !== message.sender._id) {
-                    io.to(user).emit('receiveMessage', message);
+                    chatNamespace.to(user).emit('receiveMessage', message);
                 }
             });
         });
@@ -76,7 +80,67 @@ module.exports = function (server) {
                 }
             }
 
-            io.emit('onlineUsers', onlineUsers);
+            chatNamespace.emit('onlineUsers', onlineUsers);
+        });
+    });
+
+    
+    const users = {};
+    const socketToRoom = {};
+
+    webRTCNamespace.on('connection', (socket) => {
+        console.log(`A user with the socketId: ${socket.id} is connected`);
+
+        socket.on("join_room", ({ roomID, init }) => {
+            users[roomID] ? users[roomID].push(socket.id) : users[roomID] = [socket.id];
+
+            socketToRoom[socket.id] = roomID;
+
+            init ? initiator = socket.id : webRTCNamespace.to(initiator).emit("peer_connected", socket.id);
+        });
+
+        socket.on("sending_sdp_offer", (payload) => {
+            webRTCNamespace.to(payload.userToSignal).emit("receive_sdp_offer", {
+                signal: payload.signal,
+                callerID: payload.callerID,
+            });
+        });
+
+        socket.on("sending_sdp_answer", (payload) => {
+            webRTCNamespace.to(payload.userToSignal).emit("receive_sdp_answer", {
+                signal: payload.signal,
+                callerID: payload.callerID,
+            });
+        });
+
+        socket.on("sending_file_MetaData", (payload) => {
+            webRTCNamespace.to(payload.userToSignal).emit("receive_file_MetaData", {
+                metaData: payload.metaData,
+                callerID: payload.callerID,
+            });
+        });
+
+        socket.on("successfully_store_file_MetaData", (payload) => {
+            webRTCNamespace.to(payload.userToSignal).emit("peer_successfully_store_file_MetaData", {
+                callerID: payload.callerID,
+            });
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`A user with the socketId: ${socket.id} is disconnected`);
+
+            const roomID = socketToRoom[socket.id];
+            const socketsInRoom = users[roomID];
+
+            if (socketsInRoom) {
+                const updatedSocketsInRoom = socketsInRoom.filter((id) => id !== socket.id);
+
+                users[roomID] = updatedSocketsInRoom;
+
+                delete socketToRoom[socket.id];
+
+                socket.broadcast.emit("user_left", socket.id);
+            }
         });
     });
 }
